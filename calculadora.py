8daabbf7e9f53e6f81d4ad08424ed64d849d32b4
Jonatas Coelho <jonatascoelho1993@gmail.com>
2024-06-28 18:49:11 -0300

    operação de soma implementada

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,10 @@
 print("Bem vindo a calculadora de operções basicas!")
 print("Digite o numero referente a operação desejada:")
 print("1:Soma \n2:Subtração \n3:Multiplicação \n4:Divisão")
-operacao = input("Operação: ")
-numero1 = input("Digite o primeiro numero: ")
-numero2 = input("Digite o segundo numero: ")
-print(numero1 + operacao + numero2)
+operacao = str(input("Operação: "))
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+if operacao == "1":
+    resultado = numero1 + numero2
+    print(str(numero1) + "+" + str(numero2) + "=" + str(resultado))
